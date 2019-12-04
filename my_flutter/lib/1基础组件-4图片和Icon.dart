74ad3399997f26323fb3cb2ee95c223da14fa536
欧阳芳斌 <478027478@qq.com>
2019-12-04 16:55:54 +0800

    1基础组件-4图片和Icon

---
+++ my_flutter/lib/1基础组件-4图片和Icon.dart
@@ -0,0 +1,121 @@
+//1基础组件-4图片和Icon
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var img = AssetImage("images/avatar.png");
+    String icons = "";
+    icons += "\uE914";
+    icons += " \uE000";
+    icons += " \uE90D";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          icons,
+          style: TextStyle(
+              fontFamily: "MaterialIcons", fontSize: 24.0, color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+            children: <Image>[
+          Image(
+            image: img,
+            height: 50.0,
+            width: 100.0,
+            fit: BoxFit.fill,
+          ),
+          Image(
+            image: img,
+            height: 50,
+            width: 50.0,
+            fit: BoxFit.contain,
+          ),
+          Image(
+            image: img,
+            width: 100.0,
+            height: 50.0,
+            fit: BoxFit.cover,
+          ),
+          Image(
+            image: img,
+            width: 100.0,
+            height: 50.0,
+            fit: BoxFit.fitWidth,
+          ),
+          Image(
+            image: img,
+            width: 100.0,
+            height: 50.0,
+            fit: BoxFit.fitHeight,
+          ),
+          Image(
+            image: img,
+            width: 100.0,
+            height: 50.0,
+            fit: BoxFit.scaleDown,
+          ),
+          Image(
+            image: img,
+            height: 50.0,
+            width: 100.0,
+            fit: BoxFit.none,
+          ),
+          Image(
+            image: img,
+            width: 100.0,
+            color: Colors.blue,
+            colorBlendMode: BlendMode.difference,
+            fit: BoxFit.fill,
+          ),
+          Image(
+            image: img,
+            width: 100.0,
+            height: 200.0,
+            repeat: ImageRepeat.repeatY,
+          )
+        ].map((e) {
+          return Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: SizedBox(
+                  width: 100,
+                  child: e,
+                ),
+              ),
+              Text(e.fit.toString()),
+              Icon(Icons.error, color: Colors.green),
+            ],
+          );
+        }).toList()),
+      ),
+    );
+  }
+}
