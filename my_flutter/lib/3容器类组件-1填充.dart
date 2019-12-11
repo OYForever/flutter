165b81a9b9fe04f018a0ccd8c3eb1272362294a3
欧阳芳斌 <478027478@qq.com>
2019-12-11 10:08:53 +0800

    3容器类组件-1填充

---
+++ my_flutter/lib/3容器类组件-1填充.dart
@@ -0,0 +1,69 @@
+//3容器类组件-1填充
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: PaddingTestRoute(),
+    );
+  }
+}
+
+class PaddingTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      //上下左右各添加16像素补白
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        //显式指定对齐方式为左对齐，排除对齐干扰
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            //左边添加8像素补白
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text("Hello World"),
+          ),
+          Padding(
+            //上下各添加8像素补白
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Text("I am Jack"),
+          ),
+          Padding(
+            //分别指定四个方向的补白
+            padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 20.0),
+            child: Text("Your friend"),
+          ),
+        ],
+      ),
+    );
+  }
+}
