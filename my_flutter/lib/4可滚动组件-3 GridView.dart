daa104d02274397ec6bae53dfe7408e846a16495
欧阳芳斌 <478027478@qq.com>
2019-12-30 16:15:17 +0800

    4可滚动组件-3 GridView

---
+++ my_flutter/lib/4可滚动组件-3 GridView.dart
@@ -0,0 +1,165 @@
+//4可滚动组件-3 GridView
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: InfiniteGridView(),
+    );
+  }
+}
+
+class GridView1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      // padding: EdgeInsets.fromLTRB(10, 10, 10, 10 + MediaQuery.of(context).padding.bottom),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+        // mainAxisSpacing: 10,
+        // crossAxisSpacing: 10,
+      ),
+      children: <Widget>[
+        Icon(Icons.ac_unit),
+        Icon(Icons.airport_shuttle),
+        Icon(Icons.all_inclusive),
+        Icon(Icons.beach_access),
+        Icon(Icons.cake),
+        Icon(Icons.free_breakfast),
+      ],
+    );
+  }
+}
+
+class GridView2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      childAspectRatio: 1.0,
+      children: <Widget>[
+        Icon(Icons.ac_unit),
+        Icon(Icons.airport_shuttle),
+        Icon(Icons.beach_access),
+        Icon(Icons.all_inclusive),
+        Icon(Icons.free_breakfast),
+        Icon(Icons.cake),
+      ],
+    );
+  }
+}
+
+class GridView3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 120,
+        childAspectRatio: 2.0,
+      ),
+      children: <Widget>[
+        Icon(Icons.ac_unit),
+        Icon(Icons.airport_shuttle),
+        Icon(Icons.all_inclusive),
+        Icon(Icons.beach_access),
+        Icon(Icons.cake),
+        Icon(Icons.free_breakfast),
+      ],
+    );
+  }
+}
+
+class GridView4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+      maxCrossAxisExtent: 120,
+      childAspectRatio: 2.0,
+      children: <Widget>[
+        Icon(Icons.ac_unit),
+        Icon(Icons.airport_shuttle),
+        Icon(Icons.all_inclusive),
+        Icon(Icons.cake),
+        Icon(Icons.free_breakfast),
+        Icon(Icons.beach_access),
+      ],
+    );
+  }
+}
+
+class InfiniteGridView extends StatefulWidget {
+  @override
+  _InfiniteGridViewState createState() => _InfiniteGridViewState();
+}
+
+class _InfiniteGridViewState extends State<InfiniteGridView> {
+  List<IconData> _icons = []; //保存icon数据
+
+  @override
+  void initState() {
+    super.initState();
+    _retrieveIcons();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+      ),
+      itemCount: _icons.length,
+      itemBuilder: (context, index) {
+        if (index == _icons.length - 1 && _icons.length < 200) {
+          _retrieveIcons();
+        }
+        return Icon(_icons[index]);
+      },
+    );
+  }
+
+  void _retrieveIcons() {
+    Future.delayed(Duration(milliseconds: 0)).then((e) {
+      setState(() {
+        _icons.addAll([
+          Icons.ac_unit,
+          Icons.airport_shuttle,
+          Icons.all_inclusive,
+          Icons.beach_access,
+          Icons.cake,
+          Icons.free_breakfast,
+        ]);
+      });
+    });
+  }
+}
