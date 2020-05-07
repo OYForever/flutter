e60ec25603e4db79431500aa903a196393c3b484
Zero <478027478@qq.com>
2020-05-07 08:44:46 +0800

    5功能型组件-1导航返回拦截

---
+++ my_flutter/lib/5功能型组件-1导航返回拦截.dart
@@ -0,0 +1,72 @@
+//5功能型组件-1导航返回拦截
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          //在此处添加子控件
+          WillPopScopeTestRoute(),
+        ],
+      ),
+    );
+  }
+}
+
+class WillPopScopeTestRoute extends StatefulWidget {
+  @override
+  _WillPopScopeTestRouteState createState() => _WillPopScopeTestRouteState();
+}
+
+class _WillPopScopeTestRouteState extends State<WillPopScopeTestRoute> {
+  DateTime _lastPressedAt; //上次点击的时间
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (_lastPressedAt == null ||
+            DateTime.now().difference(_lastPressedAt) > Duration(seconds: 1)) {
+          //两次点击间隔超过1秒则重新计时
+          _lastPressedAt = DateTime.now();
+          return false;
+        }
+        return true;
+      },
+      child: Container(
+        alignment: Alignment.center,
+        child: Text("1秒内连续按两次返回键退出"),
+      ),
+    );
+  }
+}
